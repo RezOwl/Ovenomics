2cea09194cbc201b09ee6c4abdae3718b28da1d5
Luckydeva <thendeanrichard29@gmail.com>
2024-05-28 09:03:35 +0700

    Mengubah tampilan edit & create di ingredient

--- resources/views/ingredients/create.blade.php
+++ resources/views/ingredients/create.blade.php
@@ -6,6 +6,7 @@
     <title>Tambah</title>
 
     {{-- css link --}}
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <link rel="stylesheet" href="css/Styles.css">
 
     {{-- Font --}}
@@ -14,51 +15,65 @@
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>Create a ingredient</h1>
-    <div>
-        @if($errors->any())
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{$error}}</li>
-            @endforeach
-        </ul>
+    <section id="header">
+        <nav class="navbar">
+            <h4>Ovenomics</h4>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">User</a></li>
+            </ul>
+        </nav>
+    </section>
 
-        @endif
+    <div class="container -ml-3 -mr-3">
+        <h1>Create a ingredient</h1>
+        <div>
+            @if($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+
+            @endif
+        </div>
+        <form method="post" action="{{route('ingredient.store')}}">
+            @csrf
+            @method('post')
+            <div>
+                <label for="name">Name</label>
+                <input type="text" name="name" placeholder="name" class="form-control">
+            </div>
+            <div>
+                <label for="description">Description</label>
+                <input type="text" name="description" placeholder="description" class="form-control">
+            </div>
+            <div>
+                <label for="category">Category</label>
+                <input type="text" name="category" placeholder="category" class="form-control">
+            </div>
+            <div>
+                <label for="type_of_unit">Type of unit</label>
+                <input type="text" name="type_of_unit" placeholder="type of unit" class="form-control">
+            </div>
+            <div>
+                <label for="quantity">Quantity</label>
+                <input type="text" name="quantity" placeholder="quantity" class="form-control">
+            </div>
+            <div>
+                <label for="unit_case">Unit case</label>
+                <input type="text" name="unit_case" placeholder="unit case" class="form-control">
+            </div>
+            <div>
+                <label for="cost">Cost</label>
+                <input type="text" name="cost" placeholder="cost" class="form-control">
+            </div>
+            <div>
+                <button onclick="window.location='{{ route('ingredient.index') }}'" class="btn btn-outline-primary">Back</button>
+                <input type="submit" value="Save a new ingredient" class="btn btn-primary" />
+            </div>
+        </form>
     </div>
-    <form method="post" action="{{route('ingredient.store')}}">
-        @csrf
-        @method('post')
-        <div>
-            <label for="name">Name</label>
-            <input type="text" name="name" placeholder="name">
-        </div>
-        <div>
-            <label for="description">Description</label>
-            <input type="text" name="description" placeholder="description">
-        </div>
-        <div>
-            <label for="category">Category</label>
-            <input type="text" name="category" placeholder="category">
-        </div>
-        <div>
-            <label for="type_of_unit">Type of unit</label>
-            <input type="text" name="type_of_unit" placeholder="type of unit">
-        </div>
-        <div>
-            <label for="quantity">Quantity</label>
-            <input type="text" name="quantity" placeholder="quantity">
-        </div>
-        <div>
-            <label for="unit_case">Unit case</label>
-            <input type="text" name="unit_case" placeholder="unit case">
-        </div>
-        <div>
-            <label for="cost">Cost</label>
-            <input type="text" name="cost" placeholder="cost">
-        </div>
-        <div>
-            <input type="submit" value="Save a new ingredient" />
-        </div>
-    </form>
 </body>
 </html>
--- resources/views/ingredients/edit.blade.php
+++ resources/views/ingredients/edit.blade.php
@@ -6,6 +6,7 @@
     <title>Edit</title>
 
     {{-- css link --}}
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <link rel="stylesheet" href="css/Styles.css">
 
     {{-- Font --}}
@@ -14,51 +15,65 @@
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>Edit a ingredient</h1>
-    <div>
-        @if($errors->any())
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{$error}}</li>
-            @endforeach
-        </ul>
+    <section id="header">
+        <nav class="navbar">
+            <h4>Ovenomics</h4>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">User</a></li>
+            </ul>
+        </nav>
+    </section>
 
-        @endif
+    <div class="container -ml-3 -mr-3">
+        <h1>Edit a ingredient</h1>
+        <div>
+            @if($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+
+            @endif
+        </div>
+        <form method="post" action="{{ route('ingredient.update', ['ingredient' => $ingredient]) }}">
+            @csrf
+            @method('put')
+            <div>
+                <label for="name">Name</label>
+                <input type="text" name="name" placeholder="name" value="{{$ingredient->name}}" class="form-control" />
+            </div>
+            <div>
+                <label for="description">Description</label>
+                <input type="text" name="description" placeholder="description" value="{{$ingredient->description}}" class="form-control" />
+            </div>
+            <div>
+                <label for="category">Category</label>
+                <input type="text" name="category" placeholder="category" value="{{$ingredient->category}}" class="form-control" />
+            </div>
+            <div>
+                <label for="type_of_unit">Type of unit</label>
+                <input type="text" name="type_of_unit" placeholder="type of unit" value="{{$ingredient->type_of_unit}}" class="form-control" />
+            </div>
+            <div>
+                <label for="quantity">Quantity</label>
+                <input type="text" name="quantity" placeholder="quantity" value="{{$ingredient->quantity}}" class="form-control" />
+            </div>
+            <div>
+                <label for="unit_case">Unit case</label>
+                <input type="text" name="unit_case" placeholder="unit case" value="{{$ingredient->unit_case}}" class="form-control" />
+            </div>
+            <div>
+                <label for="cost">Cost</label>
+                <input type="text" name="cost" placeholder="cost" value="{{$ingredient->cost}}" class="form-control" />
+            </div>
+            <div>
+                <button onclick="window.location='{{ route('ingredient.index') }}'" class="btn btn-outline-primary">Back</button>
+                <input type="submit" value="Update" class="btn btn-primary" />
+            </div>
+        </form>
     </div>
-    <form method="post" action="{{ route('ingredient.update', ['ingredient' => $ingredient]) }}">
-        @csrf
-        @method('put')
-        <div>
-            <label for="name">Name</label>
-            <input type="text" name="name" placeholder="name" value="{{$ingredient->name}}" />
-        </div>
-        <div>
-            <label for="description">Description</label>
-            <input type="text" name="description" placeholder="description" value="{{$ingredient->description}}" />
-        </div>
-        <div>
-            <label for="category">Category</label>
-            <input type="text" name="category" placeholder="category" value="{{$ingredient->category}}" />
-        </div>
-        <div>
-            <label for="type_of_unit">Type of unit</label>
-            <input type="text" name="type_of_unit" placeholder="type of unit" value="{{$ingredient->type_of_unit}}" />
-        </div>
-        <div>
-            <label for="quantity">Quantity</label>
-            <input type="text" name="quantity" placeholder="quantity" value="{{$ingredient->quantity}}" />
-        </div>
-        <div>
-            <label for="unit_case">Unit case</label>
-            <input type="text" name="unit_case" placeholder="unit case" value="{{$ingredient->unit_case}}" />
-        </div>
-        <div>
-            <label for="cost">Cost</label>
-            <input type="text" name="cost" placeholder="cost" value="{{$ingredient->cost}}" />
-        </div>
-        <div>
-            <input type="submit" value="Update" />
-        </div>
-    </form>
 </body>
 </html>
